654694cd5c3b12156982fa04f63cb073a39a2587
Jichao Ouyang <oyanglulu@gmail.com>
2015-06-17 23:57:07 +0800

    basic authenticate

--- api/hrlh.rb
+++ api/hrlh.rb
@@ -12,6 +12,9 @@ module HRLH
         @database ||= Orchestrate::Application.new(ENV['ORCH_API_KEY'],ENV['ORCH_REGION'])
       end
 
+      def auth! name, pass
+        URI.decode_www_form_component(name)==ENV['ORCH_REGION']&&pass==ENV['ORCH_API_KEY']
+      end
       def interview_db
         database['interviewer']
       end
@@ -30,6 +33,9 @@ module HRLH
     end
 
     resource :interviewer do
+      http_basic do |username, password|
+        auth! username, password
+      end
       desc "create interviewer"
       params do
         requires :name, type: String
@@ -68,6 +74,10 @@ module HRLH
     end
 
     resource :interview do
+      http_basic do |username, password|
+        auth! username, password
+      end
+      
       desc 'create interiew'
       params do
         requires :description, type: String
@@ -90,6 +100,10 @@ module HRLH
     end
 
     resource :interviewers do
+      http_basic do |username, password|
+        auth! username, password
+      end
+      
       desc 'get all interviews'
       get do
         interview_db.map{|result| to_interviewer(result)}
@@ -97,6 +111,9 @@ module HRLH
     end
 
     resource :interviews do
+      http_basic do |username, password|
+        auth! username, password
+      end
       desc 'list all interviews'
       get do
         database['interview'].map{ |interview|
