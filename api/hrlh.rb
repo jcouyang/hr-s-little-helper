7adfd10cb9e123748d3df1b98f085a913aeb820c
Jichao Ouyang <oyanglulu@gmail.com>
2015-06-17 22:00:51 +0800

    update interview

--- api/hrlh.rb
+++ api/hrlh.rb
@@ -68,23 +68,70 @@ module HRLH
     end
 
     resource :interview do
+      desc 'get interview'
+      route_param :id do
+        get do
+          interview = database['interview'][params[:id]]
+          interviewers = interview.relations[:attend_by].map{ |i|
+            {
+              key: i.key,
+              name: i.value['name'],
+              email: i.value['email']
+            }
+          }
+          interview.value['interviewers']=interviewers
+          interview.value
+        end
+        params do
+          optional :description, type: String
+          optional :name, type: String
+          optional :date, type: Date
+          optional :interviewers, type: Array
+        end
+        put do
+          interview = database[:interview][params[:id]]
+          updated = params.reject{ |key,val|val.nil?}
+            .map{ |key, val| interview[key] = val if val!=interview[key]}
+            .size
+          return 201 unless updated
+          interview.save
+          if params[:interviewers]
+            # delete old relatioins
+            interview[:interviewers].each do |id|
+              interviewer = database[:interviewer][id]
+              interview.relations[:attend_by].delete(interviewer)
+              interviewer.relations[:attend].delete(interview)
+            end
+            # build new relations
+            params[:interviewers].each do |id|
+              interviewer = database[:interviewer][id]
+              interview.relations[:attend_by] << interviewer
+              interviewer.relations[:attend] << interview
+            end
+          end
+        end
+      end
+
       desc 'create interiew'
       params do
         requires :description, type: String
         requires :name, type: String
         requires :interviewers, type: Array
+        requires :date, type: Date
       end
       post do
         resp = database.client.post('interview', {
                                       description: params[:description],
-                                      name: params[:name]
+                                      name: params[:name],
+                                      date: params[:date],
+                                      interviewers: params[:interviewers]
                                     })
         return resp if resp.status!=201
         key = resp.location.match(/interview\/(?<key>.*)\/refs/)[:key]
 
-        database['interviewer'].search("email:#{params[:interviewers].join(' or ')}").find.each do |score, interviewer|
-          interviewer.relations[:attend] << database['interview'][key]
-          database['interview'][key].relations[:attend_by] << interviewer
+        params[:interviewers].each do |id| 
+          interviewer[id].relations[:attend] << database['interview'][key]
+          database['interview'][key].relations[:attend_by] << interviewer[id]
         end
       end
     end
